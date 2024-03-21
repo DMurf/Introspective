b4e2d74e6d8528a5f8e02fa77dad0956f2597666
Dan Murfitt <dan@dmurfitt.co.uk>
2024-03-21 20:09:36 +0000

    Updates with example pages for Account and Kanban

---
+++ Introspective.Web/Controllers/BoardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Introspective.Web.Controllers
+{
+    public class BoardsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Introspective.Web/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Introspective.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
